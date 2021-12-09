b4723b1013a9a45a4237ab8771fa7ca658af641a
Katriel Albuquerque Galvão de Araújo <katrielaraujo@gmail.com>
2021-12-08 21:07:52 -0300

    Initial commit

---
+++ src/basico/AreaCirculo.java
@@ -0,0 +1,10 @@
+package basico;
+
+public class AreaCirculo {
+    public static void main(String[] args) {
+        double raio = 3.14;
+        double pi = 3.14159;
+
+        System.out.println(pi * raio * raio);
+    }
+}
